ee0f5c7f638a1a4ffff23411b49e5b922088c911
Anand Thakker <github@anandthakker.net>
2018-02-03 11:40:06 -0500

    [codemod] cjs, exports, named-export-generation
    Using 5to6-codemod/transforms/{cjs,exports,named-export-generation}.js

--- src/source/rtl_text_plugin.js
+++ src/source/rtl_text_plugin.js
@@ -1,14 +1,16 @@
 // @flow
-const Evented = require('../util/evented');
+
+import Evented from '../util/evented';
+import window from '../util/window';
 
 let pluginRequested = false;
 let pluginURL = null;
 
-module.exports.evented = new Evented();
+export const evented = new Evented();
 
 type ErrorCallback = (error: Error) => void;
 
-module.exports.registerForPluginAvailability = function(
+export const registerForPluginAvailability = function(
     callback: (args: {pluginURL: string, errorCallback: ErrorCallback}) => void
 ) {
     if (pluginURL) {
@@ -20,20 +22,20 @@ module.exports.registerForPluginAvailability = function(
 };
 
 // Only exposed for tests
-module.exports.clearRTLTextPlugin = function() {
+export const clearRTLTextPlugin = function() {
     pluginRequested = false;
     pluginURL = null;
 };
 
-module.exports.setRTLTextPlugin = function(url: string, callback: ErrorCallback) {
+export const setRTLTextPlugin = function(url: string, callback: ErrorCallback) {
     if (pluginRequested) {
         throw new Error('setRTLTextPlugin cannot be called multiple times.');
     }
     pluginRequested = true;
     pluginURL = url;
-    module.exports.errorCallback = callback;
     module.exports.evented.fire('pluginAvailable', { pluginURL: pluginURL, errorCallback: callback });
+    export const errorCallback = callback;
 };
 
-module.exports.applyArabicShaping = (null: ?Function);
-module.exports.processBidirectionalText = (null: ?(string, Array<number>) => Array<string>);
+export const applyArabicShaping = null: ?Function;
+export const processBidirectionalText = null: ?(string, Array<number>) => Array<string>;
